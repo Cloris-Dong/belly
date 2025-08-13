b6bffe35ade4cf0ba0def6c476352e805c528fb9
Han Dong <your.email@example.com>
2025-08-13 17:02:21 +0800

    test from Xcode

--- Belly/ContentView.swift
+++ Belly/ContentView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, belly!")
+        VStack(spacing: 16) {
+            Text("Hello from Xcode!")
+                .font(.title)
+                .padding()
+                .background(.yellow)
+                .cornerRadius(12)
+
+            Text("This line was edited in Xcode")
+                .font(.subheadline)
         }
         .padding()
     }
